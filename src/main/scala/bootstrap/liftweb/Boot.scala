973636e53d9f035a230953c808ee8415d418a72d
Gu Xiangguo <guxiangguo@gmail.com>
2013-07-11 20:39:09 +0800

    add fineuploader

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -44,6 +44,8 @@ class Boot {
     def sitemap = SiteMap(
       Menu.i("Home") / "index" >> User.AddUserMenusAfter, // the simple way to declare a menu
 
+      Menu.i("FileUploader") / "fileuploader", // the simple way to declare a menu
+
       // more complex because this menu allows anything in the
       // /static path to be visible
       Menu(Loc("Static", Link(List("static"), true, "/static/index"),
